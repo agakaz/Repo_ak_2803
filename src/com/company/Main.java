63137fde85f765c3bf0ad1d39a6601565729a9c8
agakaz <agnieszka.kazanowska@wp.pl>
2021-03-28 10:32:00 +0200

    Drugi commit (bez zmian w stosunku do pierwszego)

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Witajcie w pokoju 1");
+        System.out.println("To jest projekt Agnieszki.");
+        System.out.println("Linia 3");
+    }
+}
